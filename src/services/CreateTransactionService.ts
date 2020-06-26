0fcb286c553fdfc487c53a18edafe12cbdb492fb
Felipe Suri <felipeheredia1234@gmail.com>
2020-06-26 08:00:23 -0300

    :construction_worker: update create transaction service

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,50 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
 
-import Transaction from '../models/Transaction';
+import AppError from '../errors/AppError';
+
+import Transaction, { TransactionType } from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+import CreateCategoryService from './CreateCaterogyService';
+
+interface Request {
+  title: string;
+  value: number;
+  type: TransactionType;
+  categoryTitle: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    categoryTitle,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const balance = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && balance.total < value) {
+      throw new AppError(
+        'Your outcome is bigger than your total credits available.',
+        400,
+      );
+    }
+
+    const createCategoryService = new CreateCategoryService();
+    const category = await createCategoryService.execute(categoryTitle);
+
+    const transaction = transactionsRepository.create({
+      title,
+      category_id: category.id,
+      value,
+      type,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
