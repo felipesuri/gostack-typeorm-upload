91053de72117648ceb11755c6f7e99dffd315fae
Felipe Suri <felipeheredia1234@gmail.com>
2020-06-26 10:56:11 -0300

    :construction_worker: add delete transaction service

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,25 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { isUuid } from 'uuidv4';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    if (!id || !isUuid(id)) {
+      throw new AppError('Invalid transaction id', 404);
+    }
+
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transactionExists = await transactionsRepository.findOne(id);
+
+    if (!transactionExists) {
+      throw new AppError('Transaction not found', 404);
+    }
+
+    await transactionsRepository.delete(id);
   }
 }
 
