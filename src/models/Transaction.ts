234b2c14a9d9589942982aa27749399f34518861
Felipe Suri <felipeheredia1234@gmail.com>
2020-06-26 07:10:49 -0300

    :pushpin: update models

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,53 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import Category from './Category';
+
+export type TransactionType = 'income' | 'outcume';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
-  type: 'income' | 'outcome';
+  @Column({
+    type: 'enum',
+    enum: ['income', 'outcome'],
+  })
+  type: TransactionType;
 
+  @Column('int')
   value: number;
 
-  category_id: string;
+  @ManyToOne(() => Category, category => category.transactions, {
+    onDelete: 'SET NULL',
+    onUpdate: 'CASCADE',
+    eager: true,
+  })
+  @JoinColumn({
+    name: 'category_id',
+    referencedColumnName: 'id',
+  })
+  category: Category;
 
+  @CreateDateColumn({
+    type: 'timestamp',
+  })
   created_at: Date;
 
+  @UpdateDateColumn({
+    type: 'timestamp',
+  })
   updated_at: Date;
 }
 
